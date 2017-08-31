faf98d7a09a68fba6e2afaab65c061097639b425
Robocopine <julie@bottcher.ch>
2017-08-31 21:14:10 +0200

    BDD par default et page index pas très sexy

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'admin.php'; ?>
+
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <title>Titre</title>
+  </head>
+
+  <body>
+    <!-- Un petit corps tout nu pas très sexy -->
+  </body>
+
+</html>
